03bbe08c0b5cca8f1b1b57e3a3f2bc0244dd9ca6
Liam Brown <lffbrown@hotmail.co.uk>
2024-02-16 00:17:54 +0000

    Current state of analysis, working on combination extraction

--- combo/analyse_data/analyse_data.py
+++ combo/analyse_data/analyse_data.py
@@ -1,5 +1,7 @@
 import csv
 
+import numpy as np
+import pandas as pd
 from astropy.io import fits
 from astropy.table import Table
 
@@ -28,3 +30,112 @@ class Analyse():
 
         with fits.open(f'{self.selection_folder_path}/selection.fits') as hdul:
             self.all_galaxies = Table(hdul[1].data)
+
+        self.eazy_photoz_path = f'{self.selection_folder_path}/eazy_out/photoz'
+        
+        with fits.open(f'{self.eazy_photoz_path}.data.fits') as hdul:
+            self.zbest:np.ndarray = hdul[1].data
+            self.zgrid:np.ndarray = hdul[2].data
+            self.chi2:np.ndarray = hdul[3].data
+            self.coeffs:np.ndarray = hdul[4].data
+        with fits.open(f'{self.eazy_photoz_path}.zout.fits') as hdul:
+            self.zout = Table(hdul[1].data)
+
+
+    def _calc_single_combination(self, zbests, chi2s) -> tuple[float, float, np.ndarray]:
+        """Combine the given zbests and chi2s to produce either mean z, or the min of the combined chi2.
+        
+        Parameters
+        ----------
+        zbests : array_like
+            Best fit photo z values from EAZY
+        chi2 : array_like
+            The chi squared distributions for each of the galaxies.
+                Should be shaped `(galaxy, chi2)`
+
+
+        Returns
+        -------
+        z_mean : float
+            Mean of the zbests
+        z_chi2 : float
+            Min of the combined chi squared distribution
+        chi2_combo : ndarray
+            The combined chi squared distribution of the galaxies
+        """
+
+        z_mean = np.mean(zbests)
+
+        chi2_combo = np.sum(chi2s, axis=0)
+        z_chi2 = self.zgrid[np.argmin(chi2_combo)]
+
+        return z_mean, z_chi2, chi2_combo
+
+    def _data_single_combination(self, indiv_range:tuple) -> None:
+        """
+        Get all data points for comparision of integrated to combination.
+        Append to the various attributes of object
+        
+        Parameters
+        ----------
+        indiv_range : tuple
+            The range of the individual galaxies (not including the integrated)
+        """
+        # get idx of the integrated galaxy
+        int_idx = indiv_range[0] - 1
+        z_int = self.zbest[int_idx]
+        chi2_int = self.chi2[int_idx]
+
+        indiv_slice = slice(*indiv_range)
+
+        zbests = self.zbest[indiv_slice]
+        chi2s = self.chi2[indiv_slice]
+        z_mean, z_chi2, chi2_combo = self._calc_single_combination(zbests, chi2s)
+        
+        self.z_int.append(z_int)
+        self.z_mean.append(z_mean)
+        self.z_chi2.append(z_chi2)
+        self.chi2_int.append(chi2_int)
+        self.chi2_combo.append(chi2_combo)
+
+    def _create_dataframe(self) -> None:
+        self.combo_galaxies = pd.DataFrame({
+            'z_red': self.z_red,
+            'combo': self.combo,
+            'z_int': self.z_int,
+            'z_mean': self.z_mean,
+            'z_chi2': self.z_chi2,
+            'chi2_int': self.chi2_int,
+            'chi2_combo': self.chi2_combo,
+        })
+
+    def _combine(self) -> None:
+        # reset, will then be filled
+        self.z_red = []
+        self.combo = []
+        self.z_int = []
+        self.z_mean = []
+        self.z_chi2 = []
+        self.chi2_int = []
+        self.chi2_combo = []
+
+        # go through each galaxy, see if a combo or not
+        last = 0
+        for i, galaxy in enumerate(self.all_galaxies[1:], start=1):
+            if galaxy['combo'] != 1:
+                indiv_range = (last + 1, i)
+
+                # append zred and combo
+                self.z_red.append(self.all_galaxies['zred'][last])
+                self.combo.append(self.all_galaxies['combo'][last])
+                self._data_single_combination(indiv_range)
+
+                last = i
+
+        self._create_dataframe()
+
+def main() -> None:
+    pass
+
+if __name__ == '__main__':
+    main()
