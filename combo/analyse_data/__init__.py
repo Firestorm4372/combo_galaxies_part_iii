a7831f32d102eb3b78c127031f135a019ab1126c
Liam Brown <lffbrown@hotmail.co.uk>
2024-03-02 14:58:45 +0000

    Save errors to dataframe

--- combo/analyse_data/__init__.py
+++ combo/analyse_data/__init__.py
@@ -2,3 +2,4 @@ from .analyse_data import ProcessData
 from .analyse_data import Analyse
 
 from .nice_plot import SaveData
+from.nice_plot import NicePlots
--- combo/analyse_data/nice_plot.py
+++ combo/analyse_data/nice_plot.py
@@ -1,3 +1,6 @@
+import os
+
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
@@ -10,6 +13,18 @@ class SaveData(analyse_data.ProcessData):
 
     def __init__(self, folder_name: str, selection_name: str, data_path: str = '.data') -> None:
         super().__init__(folder_name, selection_name, data_path)
+
+    def _write_errors(self) -> None:
+        for z_method in ('z_int', 'z_mean', 'z_chi2'):
+            zred = np.asarray(self.df_combo['z_red'])
+            z = np.asarray(self.df_combo[z_method])
+
+            difference = z - zred
+            error = difference / (1 + zred)
+
+            loc = self.df_combo.columns.get_loc(z_method) + 1
+            colname = f'err_{z_method.split('_')[-1]}'
+            self.df_combo.insert(loc, colname, error)
     
     def save_dataframe(self) -> None:
         """
@@ -22,6 +37,8 @@ class SaveData(analyse_data.ProcessData):
         self.df_combo = self.combo_galaxies[['z_red', 'combo', 'combo_frac_err', 'z_int', 'z_mean', 'z_chi2']]
         self.df_combo.rename(columns={'combo_frac_err': 'frac_err'}, inplace=True)
         self.df_combo.index.name = 'idx'
+
+        self._write_errors()
 
         self.dataframe_path = f'{self.selection_folder_path}/combo_galaxies.csv'
         self.df_combo.to_csv(self.dataframe_path)
@@ -38,7 +55,8 @@ class NicePlots():
     figure_save_path : str, default None
         Where to save produced figures.
         If default of None, will save in same directory as `combo_galaxies_file_path` in folder called `figures`.
-        (Doesn't include final `/`)
+        If provided, expects to have been created.
+        (Doesn't include final `/`).
     """
 
     def __init__(self, combo_galaxies_file_path:str, figure_save_path:str=None) -> None:
@@ -52,6 +70,7 @@ class NicePlots():
         # set figure save path
         if figure_save_path == None:
             self.figure_save_path = f'{self.folder_path}/figures'
+            os.makedirs(self.figure_save_path, exist_ok=True)
         else:
             self.figure_save_path = figure_save_path
 
