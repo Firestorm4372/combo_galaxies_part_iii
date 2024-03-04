846c39e04df8fa7d9ae8ba9c5f340d87310dc7eb
Liam Brown <lffbrown@hotmail.co.uk>
2024-03-04 17:21:38 +0000

    Correct init file

--- combo/analyse_data/__init__.py
+++ combo/analyse_data/__init__.py
@@ -1,5 +1,6 @@
 from .analyse_data import ProcessData
 from .analyse_data import Analyse
+from .analyse_data import SedPlot
 
 from .nice_plot import SaveData
-from.nice_plot import NicePlots
+from .nice_plot import NicePlots
